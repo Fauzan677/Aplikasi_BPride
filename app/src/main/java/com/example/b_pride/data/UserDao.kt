18ae654058a791a1984395d03b172fb3ac122b88
Fauzan677 <habibifauzan677@gmail.com>
2023-10-19 22:35:40 +0700

    Menambahkan Register dan Room Database

---
+++ app/src/main/java/com/example/b_pride/data/UserDao.kt
@@ -0,0 +1,28 @@
+package com.example.b_pride.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user_table")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user_table WHERE id LIKE :id LIMIT 1")
+    suspend fun findByRoll(id: Int): User
+
+    @Query("SELECT * FROM user_table WHERE id=:user_id")
+    suspend fun getUser(user_id: Int) : User
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Delete
+    suspend fun delete(user: User)
+
+    @Query("DELETE FROM user_table")
+    suspend fun deleteAll()
+}
